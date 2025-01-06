d13df0bd99c37a83c5c2a9a77740988f2c936d82
Patrick <98470284+PatrickKish1@users.noreply.github.com>
2025-01-06 00:12:32 +0000

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ class TradingAIServer {
     // Basic middleware
     this.app.use(express.json());
     this.app.use(cors({
-      origin: process.env.ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000', 'https://pulsetrade-app.vercel.app'],
+      origin: '*',
       methods: ['GET', 'POST'],
       allowedHeaders: ['Content-Type', 'Authorization']
     }));
